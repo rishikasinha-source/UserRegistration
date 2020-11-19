4926623ab81a6f23224cbec7cd47213c910a06f5
rishika7-hub <srishika970@gmail.com>
2020-11-19 16:49:31 +0530

    LastName

--- src/UserRegistration.java
+++ src/UserRegistration.java
@@ -19,6 +19,23 @@ public class UserRegistration {
 			
 		}
 	
+	//Last Name Validation
+	public void nameValidator1() {
+		String pattern= "^[A-Z][a-z]{2,}$";
+		System.out.println("Enter the Last Name:");
+		Scanner scanner=new Scanner(System.in);
+		String name=scanner.next();
+		if(name.matches(pattern)) {
+			System.out.println("Valid Last Name");
+		}
+			else {
+				System.out.println("Invalid Last Name");
+				nameValidator1();
+			}
+			
+		}
+	
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,6 +43,7 @@ public class UserRegistration {
 		
 		UserRegistration user=new UserRegistration();
 		user.nameValidator();
+		user.nameValidator1();
 
 	}
 
